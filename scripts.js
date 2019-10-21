525d709c0329ad7e4114fc0a4f3422d173d3106d
Casey Idsinga and Marcus Jensen <casey@>
2019-10-21 14:24:45 -0700

    js for first and last character capitalization

---
+++ scripts.js
@@ -0,0 +1,36 @@
+var input = prompt("Please enter a sentence");
+
+
+// var manipulate = function () {
+// var last = input.length - 1;
+// var makeUpper2 = input.charAt(last).toUpperCase();
+// var makeUpper = input.charAt(0).toUpperCase();
+//
+// console.log(makeUpper + makeUpper2);
+//
+// };
+// manipulate();
+//
+// var stuff = fucntion () {
+// var last = input.length - 1;
+// var makeUpper2 =
+// }
+
+//asks person for sting
+
+//collect first character
+
+//collect last character
+  // -how many characters are in inputted sting
+
+//log result to the console
+
+var stuff = function() {
+var firstChar = input.charAt(0).toUpperCase();
+var lastChar = input.length -1;
+var dog = input.charAt(lastChar).toUpperCase();
+
+console.log(firstChar + dog);
+}
+
+stuff();
